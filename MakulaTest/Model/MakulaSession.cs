29bad0dc5995effbfedf7420c69970a92e5c05d6
Andreas Nimptsch <animptsch@gmx.de>
2020-05-16 17:02:24 +0200

    Current Version

---
+++ MakulaTest/Model/MakulaSession.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MakulaTest.Model
+{
+    public class MakulaSession
+    {
+        public DateTime TimeStamp { get; set; }
+
+        public List<Point> Points { get; set; }
+
+        public MakulaSession()
+        {
+            TimeStamp = DateTime.Now;
+            Points = new List<Point>();
+        }
+
+    }
+}
