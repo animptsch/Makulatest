85b077e3536a76bbe9b33f1d8c20cf0a67e8926f
animptsch <animptsch>
2020-06-14 06:31:04 +0200

    CodeCleaning

--- MakulaTest/MacularDiagnosisControl.xaml.cs
+++ MakulaTest/MacularDiagnosisControl.xaml.cs
@@ -31,14 +31,14 @@ namespace MakulaTest
         private double _centerY;
         private Storyboard _sbTranslate;
         private MakulaSession _session;
-        
-        private bool _isMeasureStarted;        
+
+        private bool _isMeasureStarted;
 
         public MacularDiagnosisControl()
         {
             InitializeComponent();
             SettingsModel = new Model.Settings();
-            _isMeasureStarted = false;            
+            _isMeasureStarted = false;
         }
 
         public void SetSize(double width, double height)
@@ -51,7 +51,7 @@ namespace MakulaTest
             MyRectangle.Width = width - MyRectangle.Margin.Left - MyRectangle.Margin.Right;
 
             drawLines();
-            drawCenterCircle();            
+            drawCenterCircle();
         }
 
         private double _lastPos;
@@ -62,19 +62,17 @@ namespace MakulaTest
             set
             {
                 _lastPos = value;
-                txtLastPos.AppendText(string.Format("{0:00.00}\n", value));                
+                txtLastPos.AppendText(string.Format("{0:00.00}\n", value));
                 txtLastPos.ScrollToEnd();
             }
         }
-
-
 
         public const int CircleSize = 8;
         public readonly Brush CircleColor = new SolidColorBrush(Colors.Red);
 
         public Model.Settings SettingsModel { get; set; }
         public MainWindow Parent { get; set; }
-        
+
 
         public void StartDiagnosis()
         {
@@ -102,42 +100,23 @@ namespace MakulaTest
 
                 Point pt = getPointInOuterCircle();
                 _ellipse = moveCircle(pt, SettingsModel.Backward);
-            }           
-        }
-
-        private void Line_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            Point p = Mouse.GetPosition(MyCanvas);
-            var line = sender as Line;
-            line.Stroke = Brushes.Blue;
-
-            double m = (line.Y1 - _centerY) / (_centerX - line.X1);
-            if (Math.Abs(m) < 10)
-            { //Console.WriteLine("m=" + m.ToString());
-              line.Y2 = _centerY - (p.X - _centerX) * m;
-              line.X2 = p.X;
-            }
-            else
-            { m = (_centerX - line.X1) / (line.Y1 - _centerY);
-              //Console.WriteLine("m=" + m.ToString());
-              line.X2 = (_centerY - p.Y) * m + _centerX;
-              line.Y2 = p.Y;
             }
         }
 
+        
         private void resetTimer()
         {
             Point pt = getPointInOuterCircle();
 
             if (_moveTimer != null && _removeTimer != null && _isMeasureStarted)
-           {
-             _moveTimer.Stop();
-             _removeTimer.Stop();
-             _removeTimer.Start();
-             _moveTimer.Start();
-             
-             _ellipse = moveCircle(pt, SettingsModel.Backward);
-           }
+            {
+                _moveTimer.Stop();
+                _removeTimer.Stop();
+                _removeTimer.Start();
+                _moveTimer.Start();
+
+                _ellipse = moveCircle(pt, SettingsModel.Backward);
+            }
         }
 
 
@@ -152,7 +131,7 @@ namespace MakulaTest
 
             _session.Points.Add(new Point(EmptyValue, EmptyValue));
             UpdateTextBox();
-            
+
             resetAnimation();
             resetTimer();
         }
@@ -169,7 +148,7 @@ namespace MakulaTest
             {
                 MyCanvas.Children.Remove(_ellipse);
             }
-            
+
         }
 
         private void _removeTimer_Tick(object sender, EventArgs e)
@@ -183,15 +162,7 @@ namespace MakulaTest
 
 
         private Ellipse moveCircle(Point begin, bool backward)
-        {
-            var ellipse = new Ellipse()
-            {
-                Width = CircleSize,
-                Height = CircleSize,
-                Stroke = CircleColor,
-                Fill = CircleColor
-            };
-
+        {           
             Point end;
             if (!backward)
             {
@@ -203,9 +174,9 @@ namespace MakulaTest
                 begin = new Point(_centerX, _centerY);
             }
             
-            ellipse.SetValue(Canvas.LeftProperty, begin.X - CircleSize/2);
-            ellipse.SetValue(Canvas.TopProperty, begin.Y - CircleSize / 2);
-            MyCanvas.Children.Add(ellipse);
+            Point pt = new Point(begin.X - CircleSize / 2, begin.Y - CircleSize / 2);
+
+            var ellipse = drawCircle(pt, CircleColor);            
 
             _sbTranslate = new Storyboard();
             var daTranslateX = new DoubleAnimation();
@@ -228,8 +199,8 @@ namespace MakulaTest
             Storyboard.SetTargetProperty(daTranslateY, new PropertyPath("(UIElement.RenderTransform).(TranslateTransform.Y)"));
 
             //animate the item to the center of the screen
-            daTranslateX.To = end.X - begin.X ;
-            daTranslateY.To = end.Y - begin.Y ;
+            daTranslateX.To = end.X - begin.X;
+            daTranslateY.To = end.Y - begin.Y;
 
             if (_isMeasureStarted)
             {
@@ -239,7 +210,7 @@ namespace MakulaTest
             {
                 int i = 0;
             }
-           
+
             return ellipse;
         }
 
@@ -248,8 +219,8 @@ namespace MakulaTest
             if (_sbTranslate != null && _ellipse != null)
             {
                 Point relativePoint = _ellipse.TransformToAncestor(MyCanvas)
-                                              .Transform(new Point(CircleSize / 2.0, CircleSize / 2.0));                
-                
+                                              .Transform(new Point(CircleSize / 2.0, CircleSize / 2.0));
+
                 _session.Points.Add(relativePoint);
                 UpdateTextBox();
                 _sbTranslate.Stop();
@@ -266,8 +237,8 @@ namespace MakulaTest
         {
             _isMeasureStarted = false;
             _moveTimer.Stop();
-            _removeTimer.Stop();            
-            
+            _removeTimer.Stop();
+
             _moveTimer = null;
             _removeTimer = null;
 
@@ -284,12 +255,12 @@ namespace MakulaTest
 
             drawLines();
             drawCenterCircle();
-            
+
             _polygon = new Polygon();
 
             var removedPoints = (from p in _session.Points
-                                where p.X == EmptyValue && p.Y == EmptyValue
-                                select p).ToList();
+                                 where p.X == EmptyValue && p.Y == EmptyValue
+                                 select p).ToList();
 
             foreach (var p in removedPoints)
             {
@@ -315,213 +286,101 @@ namespace MakulaTest
             _session.Points.Clear();
         }
 
-         private void drawCircle(Point pt)
+        private Ellipse drawCircle(Point pt, Brush color)
         {
             var ellipse = new Ellipse()
             {
                 Width = CircleSize,
                 Height = CircleSize,
-                Stroke = CircleColor,
-                Fill = CircleColor
+                Stroke = color,
+                Fill = color
             };
 
-            ellipse.SetValue(Canvas.LeftProperty, pt.X );
+            ellipse.SetValue(Canvas.LeftProperty, pt.X);
             ellipse.SetValue(Canvas.TopProperty, pt.Y);
             MyCanvas.Children.Add(ellipse);
+
+            return ellipse;
         }
 
         private Point getPointInOuterCircle()
         {
             double width = MyRectangle.Width;
             double height = MyRectangle.Height;
-      
+
             Point pt, ptTan;
             var rnd = new Random();
 
             double pos = 1.0 / (double)SettingsModel.Steps;
             _pathGeo.GetPointAtFractionLength(LastPos, out pt, out ptTan);
             LastPos += pos;
-            if (LastPos >= 1.0 +_offset + pos)
-              StopDiagnosis();
+            if (LastPos >= 1.0 + _offset + pos)
+                StopDiagnosis();
 
             return pt;
         }
 
-        
-
-        private void drawOuterCircle()
-        {
-            var ellipse = new Ellipse()
-            {
-                Width = 708.0,
-                Height = 708.0,
-                Stroke = new SolidColorBrush(Colors.Red),
-            };
-
-            ellipse.SetValue(Canvas.LeftProperty, -50.0);
-            ellipse.SetValue(Canvas.TopProperty, -50.0);
-            MyCanvas.Children.Add(ellipse);
-        }
-
-
-        private void drawLinesOld(bool isDrawAmselGrid = false)
+        private void drawLines(bool isDrawAmselGrid = true)
         {
             double width = MyRectangle.Width;
             double height = MyRectangle.Height;
             double x = MyRectangle.Margin.Left;
             double y = MyRectangle.Margin.Top;
-            _centerY = _centerX = 0.0;
+
+            _centerX = width / 2.0 + x;
+            _centerY = height / 2.0 + y;
 
             clearCanvas();
-            double deltaHorz = width / LineNumber;
-            double deltaVert = height / LineNumber;
 
-            double lineHeight = y + deltaVert, lineWidth = x + deltaHorz;
+            //draw horizontal Line
+            double thickness = 3;
 
-            int centerIndex = LineNumber / 2;
-
-            //draw horizontal Lines
-            for (int i = 0; i < LineNumber; i++)
+            if (isDrawAmselGrid)
             {
-                double thickness;
-                if (centerIndex == i)
+                var line1 = new Line()
                 {
-                    _centerY = lineHeight;
-                    thickness = 3;
-                }
-                else
+                    Stroke = new SolidColorBrush(Colors.Black),
+                    Fill = new SolidColorBrush(Colors.Black),
+                    StrokeThickness = thickness,
+                    X1 = x,
+                    X2 = width + x,
+                    Y1 = _centerY,
+                    Y2 = _centerY
+                };
+
+                MyCanvas.Children.Add(line1);
+
+                //draw vertical Lines
+                var line2 = new Line()
                 {
-                    thickness = 1;
-                }
+                    Stroke = new SolidColorBrush(Colors.Black),
+                    StrokeThickness = thickness,
+                    Fill = new SolidColorBrush(Colors.Black),
+                    X1 = _centerX,
+                    X2 = _centerX,
+                    Y1 = y,
+                    Y2 = height + y
+                };
 
-                if (isDrawAmselGrid || centerIndex ==i)
-                {
-                    var line = new Line()
-                    {
-                        Stroke = new SolidColorBrush(Colors.Black),
-                        Fill = new SolidColorBrush(Colors.Black),
-                        StrokeThickness = thickness,
-                        X1 = x,
-                        X2 = width + x,
-                        Y1 = lineHeight,
-                        Y2 = lineHeight
-                    };
+                MyCanvas.Children.Add(line2);
 
-                    MyCanvas.Children.Add(line);
-                }
-                
-                lineHeight += deltaVert;
-            }
-
-            //draw vertical Lines
-            for (int i = 0; i < LineNumber; i++)
-            {
-                double thickness;
-
-                if (centerIndex == i)
-                {
-                    _centerX = lineWidth;
-                    thickness = 3;
-                }
-                else
-                {
-                    thickness = 1;
-                }
-
-                if (isDrawAmselGrid || centerIndex == i)
-                {
-                    var line = new Line()
-                    {
-                        Stroke = new SolidColorBrush(Colors.Black),
-                        StrokeThickness = thickness,
-                        Fill = new SolidColorBrush(Colors.Black),
-                        X1 = lineWidth,
-                        X2 = lineWidth,
-                        Y1 = y,
-                        Y2 = height + y
-                    };
-
-                    MyCanvas.Children.Add(line);
-                }
-                              
-                lineWidth += deltaHorz;
-
-                EllipseGeometry geo = new EllipseGeometry(new Point(_centerX, _centerY), width/2+30, height/2+30);
+                EllipseGeometry geo = new EllipseGeometry(new Point(_centerX, _centerY), width / 2 + 30, height / 2 + 30);
                 _pathGeo = geo.GetFlattenedPathGeometry();
             }
         }
 
-    private void drawLines(bool isDrawAmselGrid = true)
-    {
-      double width = MyRectangle.Width;
-      double height = MyRectangle.Height;
-      double x = MyRectangle.Margin.Left;
-      double y = MyRectangle.Margin.Top;
 
-      _centerX = width / 2.0 + x;
-      _centerY = height / 2.0 + y;
-
-  clearCanvas();
-
-      //draw horizontal Line
-      double thickness = 3;
-
-      if (isDrawAmselGrid)
-      {
-        var line1 = new Line()
-        {
-          Stroke = new SolidColorBrush(Colors.Black),
-          Fill = new SolidColorBrush(Colors.Black),
-          StrokeThickness = thickness,
-          X1 = x,
-          X2 = width + x,
-          Y1 = _centerY,
-          Y2 = _centerY
-        };
-
-        MyCanvas.Children.Add(line1);
-
-        //draw vertical Lines
-        var line2 = new Line()
-        {
-          Stroke = new SolidColorBrush(Colors.Black),
-          StrokeThickness = thickness,
-          Fill = new SolidColorBrush(Colors.Black),
-          X1 = _centerX,
-          X2 = _centerX,
-          Y1 = y,
-          Y2 = height + y
-        };
-
-        MyCanvas.Children.Add(line2);
-
-        EllipseGeometry geo = new EllipseGeometry(new Point(_centerX, _centerY), width / 2 + 30, height / 2 + 30);
-        _pathGeo = geo.GetFlattenedPathGeometry();
-      }
-    }
-
-
-    private void clearCanvas()
+        private void clearCanvas()
         {
             MyCanvas.Children.Clear();
             MyCanvas.Children.Add(MyRectangle);
         }
 
         private void drawCenterCircle()
-        {
-            var ellipse = new Ellipse()
-            {
-                Width = CircleSize,
-                Height = CircleSize,
-                Stroke = new SolidColorBrush(Colors.Black),
-                Fill = new SolidColorBrush(Colors.Black),
-                //HorizontalAlignment = HorizontalAlignment.Center,
-                //VerticalAlignment = VerticalAlignment.Center,
-            };
- 
-            ellipse.SetValue(Canvas.LeftProperty, _centerX- CircleSize / 2.0);
-            ellipse.SetValue(Canvas.TopProperty, _centerY - CircleSize / 2.0);
-            MyCanvas.Children.Add(ellipse);
+        {            
+            Point pt = new Point(_centerX - CircleSize / 2.0, _centerY - CircleSize / 2.0);
+
+            var ellipse = drawCircle(pt, new SolidColorBrush(Colors.Black));                       
         }
 
         private void MyCanvas_MouseDown(object sender, MouseButtonEventArgs e)
@@ -540,7 +399,7 @@ namespace MakulaTest
                 }
                 cancelMovement();
             }
-          
+
         }
 
         private void UpdateTextBox()
@@ -556,11 +415,11 @@ namespace MakulaTest
 
         private void MyCanvas_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
         {
-            if (e.Delta < 0 && LastPos > _offset )
+            if (e.Delta < 0 && LastPos > _offset)
             {
                 double pos = 2.0 / (double)SettingsModel.Steps;
                 LastPos -= pos;
-                
+
                 var lastPoint = _session.Points.Last();
                 if (lastPoint != null)
                 {
@@ -568,13 +427,13 @@ namespace MakulaTest
                     UpdateTextBox();
                 }
             }
-            
+
             resetAnimation();
             resetTimer();
-                        
+
             e.Handled = true;
         }
 
-        
+
     }
 }
